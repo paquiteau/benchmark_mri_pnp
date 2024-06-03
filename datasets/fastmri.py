b73e49e2abacf93a1558cf03bc4aa1c6f1b5e920
BenjaminisCoding <benjamin.jean.lapostolle@gmail.com>
2024-06-03 12:21:12 +0200

    removed unnecessary functions

--- datasets/fastmri.py
+++ datasets/fastmri.py
@@ -58,48 +58,8 @@ class fastMRI(BaseDataset):
             y_hat = torch.cat(y_hat, dim = 0).unsqueeze(0)
 
             return dict(kspace_data=y, kspace_data_hat=y_hat, target=target_torch, images=images, smaps=Smaps, mask=mask)
-        
-# def get_data(idx = 0, Smaps = False, physics = None):
 
-#     # data_transform = T.UnetDataTransform(which_challenge="multicoil")
-#     data_transform = ClassicDataTransform(which_challenge="multicoil", Smaps = Smaps, physics = physics)
-#     dataset = mri_data.SliceDataset(
-#         root=pathlib.Path(path),
-#         transform=data_transform,
-#         challenge='multicoil'
-#     )
 
-#     dataloader = torch.utils.data.DataLoader(dataset)
-#     i = -1
-#     for batch in dataloader:
-#         if idx != i:
-#             i += 1
-#             continue    
-#         target_torch, images, y, y_hat, Smaps, mask = batch
-#         y_hat = torch.cat(y_hat, dim = 0).unsqueeze(0)
-
-#         return target_torch, images, y, y_hat, Smaps, mask
-
-def get_images_coils(kspace, target, c_abs = False):
-
-    kspace_torch = to_tensor(kspace)
-    images = fastmri.ifft2c(kspace_torch)
-
-    if target is not None:
-        crop_size = (target.shape[-2], target.shape[-1])
-    else:
-        crop_size = (attrs["recon_size"][0], attrs["recon_size"][1])
-    
-    # check for FLAIR 203
-    if images.shape[-2] < crop_size[1]:
-        crop_size = (images.shape[-2], images.shape[-2])
-    
-    images = complex_center_crop(images, crop_size)
-    
-    # absolute value
-    if c_abs:
-        images = fastmri.complex_abs(images)
-    return images
 
 def to_complex_tensor(tensor):
     # Check if the tensor has the expected shape
@@ -114,36 +74,7 @@ def to_complex_tensor(tensor):
     complex_tensor = torch.complex(real_part, imaginary_part)
     
     return complex_tensor
-
-
-
-
-
-
-
-# def get_Smaps(image, images, physics):
-
-#     images_c = to_complex_tensor(images)
-#     X, Y, Back = corrupt_coils(images_c, physics)    
-#     images_c_2dim = to_tensor(images_c)
-#     Smaps = torch.sqrt(images_c_2dim ** 2 / torch.sum(images_c_2dim ** 2, dim = 1))
-#     Smaps_c = to_complex_tensor(Smaps)
-#     mask = torch.Tensor(produce_mask(image)).unsqueeze(0).repeat(20,1,1).unsqueeze(0)
-#     Smaps_c_final = Smaps_c * mask
-#     return Smaps_c_final.squeeze(0).numpy()
-
-# def produce_mask(image):
-    
-#     ### could use Otsu instead of approxiamte quantile 
-#     q = np.quantile(image, 0.6)
-#     image_bin = image.copy()
-#     image_bin[image > q] = 1
-#     image_bin[image <= q] = 0
-#     contours, _ = cv2.findContours(image_bin.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#     brain_mask = cv2.drawContours(np.zeros_like(image_bin.astype(np.uint8)), contours, -1, (255), thickness=cv2.FILLED)
-#     return brain_mask
-
-    
+   
 class ClassicDataTransform:
     """
     Data Transformer for training U-Net models.
