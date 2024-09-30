9bc31baba34a43b2b1591a5a5f2f9864dbd3d180
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2024-09-30 18:02:39 +0200

    feat: add ppnp implementation (untested)

--- solvers/pnp.py
+++ solvers/pnp.py
@@ -7,8 +7,9 @@ import numpy as np
 with safe_import_context() as import_ctx:
     import torch
     from deepinv.models import WaveletDictDenoiser
+    from deepinv.optim.optim_iterators import OptimIterator, fStep
     from deepinv.optim.data_fidelity import L2
-    from deepinv.optim import optim_builder, PnP, Prior
+    from deepinv.optim import optim_builder, PnP, Prior, BaseOptim
     from benchmark_utils.utils import stand
     from benchmark_utils.drunet import DRUNet
 
@@ -28,7 +29,7 @@ class Solver(BaseSolver):
     sampling_strategy = "callback"
     requirements = ["deepinv", "mrinufft[gpunufft]"]
     parameters = {
-        "iteration": ["PGD", "FISTA"],
+        "iteration": ["PGD", "FISTA", "ppnp-static", "ppnp-cheby", "ppnp-dynamic"],
         "prior": ["drunet", "drunet-denoised"],
     }
     max_iter = 20
@@ -63,17 +64,36 @@ class Solver(BaseSolver):
             "g_param": 0.1,
             "stepsize": 1 / self.physics.nufft.get_lipschitz_cst(10),
         }
+        kwargs_optim["early_stop"] = False
+        kwargs_optim["verbose"] = False
+        kwargs_optim["custom_init"] = get_custom_init
+        kwargs_optim["max_iter"] = self.max_iter
 
-        self.algo = optim_builder(
-            iteration=self.iteration,
-            prior=prior,
-            data_fidelity=L2(),
-            early_stop=False,
-            custom_init=get_custom_init,
-            max_iter=self.max_iter,
-            verbose=False,
-            **kwargs_optim,
-        )
+        if "pnpp" in self.iteration:
+            _, precond = self.iteration.split("-")
+            df = L2()
+            iterator = PreconditionedPnP(
+                preconditioner=precond,
+                prior=prior,
+                data_fidelity=df,
+                has_cost=False,
+                F_fn=None,
+                g_first=False,
+            )
+            self.algo = BaseOptim(
+                iterator,
+                has_cost=iterator.has_cost,
+                data_fidelity=df,
+                **kwargs_optim,
+            )
+
+        else:
+            self.algo = optim_builder(
+                iteration=self.iteration,
+                prior=prior,
+                data_fidelity=L2(),
+                **kwargs_optim,
+            )
 
     def run(self, callback):
         with torch.no_grad():
@@ -162,3 +182,111 @@ def get_DPIR_params(s1=0.5, s2=0.1, lamb=2, n_iter=10):
     sigma_denoiser = np.logspace(np.log10(s1), np.log10(s2), n_iter).astype(np.float32)
     stepsize = (sigma_denoiser / max(0.01, noise_level_img)) ** 2
     return {"lambda": lamb, "g_param": list(sigma_denoiser), "stepsize": list(stepsize)}
+
+
+class PreconditionedPnP(OptimIterator):
+    """Implement the preconditioned PnP algorithm from Hong et al. 2024."""
+
+    def __init__(self, preconditioner="dynamic", **kwargs):
+        super().__init__(**kwargs)
+        self.g_step = gStepPGD(**kwargs)
+        self.f_step = fStepPGD(**kwargs)
+        self.requires_prox_g = True
+        if self.g_first:
+            raise ValueError(
+                "The preconditioned PnP algorithm should start with a step on f."
+            )
+
+        self.preconditionner = Precond(preconditioner)
+
+    def forward(self, X, cur_data_fidelity, cur_prior, cur_params, y, physics):
+        x_prev, grad_f_prev, precond_prev = X["est"]
+        k = 0 if "it" not in X else X["it"]
+
+        # TODO add the preconditioner step
+        grad_f = self.f_step(
+            z_prev, cur_data_fidelity, cur_params, y, physics, self.preconditioner
+        )
+
+        grad_f_precond = self.preconditioner.update_grad(
+            cur_params, physics, grad_f, grad_f_prev, x, x_prev
+        )
+        z = x - cur_params["stepsize"] * grad_f_precond
+
+        x = self.g_step(z, cur_prior, cur_params)
+        z = x + alpha * (x - x_prev)
+        F = (
+            self.F_fn(x, cur_data_fidelity, cur_prior, cur_params, y, physics)
+            if self.has_cost
+            else None
+        )
+
+        return {"est": (x, grad_f, precond), "cost": F, "it": k + 1}
+
+
+class Precond:
+    def __init__(self, name, theta1=0.2, theta2=2, delta=1 / 1.633):
+        self.it = 0
+        self.name = name
+
+    def get_alpha(self, s, m):
+        alphas = np.linspace(0, 1, 1000)
+        # line search of alpha
+
+    def update_grad(self, cur_params, physics, grad, *args, **kwargs):
+        if self.name == "static":
+            grad = self._update_grad_static(physics, grad)
+
+    def _update_grad_static(self, cur_params, physics, grad_f, *args, **kwargs):
+        """update the gradient with the static preconditioner"""
+
+        alpha = cur_params["stepsize"]
+        grad_f_preconditioned = physics.A_adjoint(physics.A(grad_f))
+
+        grad_f_preconditioned *= -alpha
+        grad_f_preconditioned += 2 * grad_f
+
+        return grad_f_preconditioned
+
+    def _update_grad_cheby(self, cur_params, physics, grad_f):
+        """update the gradient with the static cheby preconditioner"""
+
+        alpha = cur_params["stepsize"]
+        grad_f_preconditioned = physics.A_adjoint(physics.A(grad_f))
+        grad_f_preconditioned *= -(10 / 3) * alpha
+        grad_f_preconditioned += 4 * grad_f
+        return grad_f_preconditioned
+
+    def _update_grad_dynamic(self, cur_params, physics, grad_f, grad_f_prev, x, x_prev):
+        """update the gradient with the dynamic preconditioner"""
+
+        s = x - x_prev
+        m = grad_f - grad_f_prev
+
+        # precompute dot products
+        sf = s.squeeze(0).squeeze(0).reshape(-1)
+        mf = m.squeeze(0).squeeze(0).reshape(-1)
+
+        ss = sf.dot(sf)
+        sm = sf.dot(mf)
+        mm = mf.dot(mf)
+
+        for a in np.linspace(0, 1, 1000):
+            sv = a * ss + (1 - a) * sm
+            vv = (a**2) * ss + ((1 - a) ** 2) * mm + (2 * a * (1 - a)) * sm
+            if sv / ss >= self.theta1 and v.dot(v) / sv <= self.theta2:
+                break
+        v = a * s + (1 - a) * m
+
+        tau = ss / sv - torch.sqrt((ss / sv) ** 2 - ss / vv)
+
+        tmp = sv - tau * vv
+        grad_f_preconditioned = tau * grad_f
+
+        if tmp >= self.delta * torch.sqrt(tmp * vv):
+            u = sf - tau * vf
+            u = u.dot(grad_f.squeeze(0).squeeze(0).reshape(-1)) * u
+            u = u.reshape(grad_f.shape)
+
+            grad_f_preconditioned += u / tmp
+        return grad_f_preconditioned
